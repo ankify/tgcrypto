992da1b86c1cc3cef84eedbc7f02fdf37747af0c
AnKiT <79446681+ankify@users.noreply.github.com>
2025-03-25 15:35:30 +0530

    Update tgcrypto.c

--- tgcrypto/tgcrypto.c
+++ tgcrypto/tgcrypto.c
@@ -1,35 +1,11 @@
-/*
- * Pyrogram - Telegram MTProto API Client Library for Python
- * Copyright (C) 2017-present Dan <https://github.com/delivrance>
- *
- * This file is part of Pyrogram.
- *
- * Pyrogram is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Pyrogram is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #define PY_SSIZE_T_CLEAN
-
 #include <Python.h>
-
 #include "aes256.h"
 #include "ige256.h"
 #include "ctr256.h"
 #include "cbc256.h"
 
-#define DESCRIPTION "Fast and Portable Cryptography Extension Library for Pyrogram\n" \
-    "TgCrypto is part of Pyrogram, a Telegram MTProto library for Python\n" \
-    "You can learn more about Pyrogram here: https://pyrogram.org\n"
+#define DESCRIPTION "Fast and Portable Cryptography Extension Library for Pyrogram"
 
 static PyObject *ige(PyObject *args, uint8_t encrypt) {
     Py_buffer data, key, iv;
@@ -39,23 +15,8 @@ static PyObject *ige(PyObject *args, uint8_t encrypt) {
     if (!PyArg_ParseTuple(args, "y*y*y*", &data, &key, &iv))
         return NULL;
 
-    if (data.len == 0) {
-        PyErr_SetString(PyExc_ValueError, "Data must not be empty");
-        return NULL;
-    }
-
-    if (data.len % 16 != 0) {
-        PyErr_SetString(PyExc_ValueError, "Data size must match a multiple of 16 bytes");
-        return NULL;
-    }
-
-    if (key.len != 32) {
-        PyErr_SetString(PyExc_ValueError, "Key size must be exactly 32 bytes");
-        return NULL;
-    }
-
-    if (iv.len != 32) {
-        PyErr_SetString(PyExc_ValueError, "IV size must be exactly 32 bytes");
+    if (data.len == 0 || data.len % 16 != 0 || key.len != 32 || iv.len != 32) {
+        PyErr_SetString(PyExc_ValueError, "Invalid data, key, or IV size");
         return NULL;
     }
 
@@ -89,28 +50,8 @@ static PyObject *ctr256_encrypt(PyObject *self, PyObject *args) {
     if (!PyArg_ParseTuple(args, "y*y*y*y*", &data, &key, &iv, &state))
         return NULL;
 
-    if (data.len == 0) {
-        PyErr_SetString(PyExc_ValueError, "Data must not be empty");
-        return NULL;
-    }
-
-    if (key.len != 32) {
-        PyErr_SetString(PyExc_ValueError, "Key size must be exactly 32 bytes");
-        return NULL;
-    }
-
-    if (iv.len != 16) {
-        PyErr_SetString(PyExc_ValueError, "IV size must be exactly 16 bytes");
-        return NULL;
-    }
-
-    if (state.len != 1) {
-        PyErr_SetString(PyExc_ValueError, "State size must be exactly 1 byte");
-        return NULL;
-    }
-
-    if (*(uint8_t *) state.buf > 15) {
-        PyErr_SetString(PyExc_ValueError, "State value must be in the range [0, 15]");
+    if (data.len == 0 || key.len != 32 || iv.len != 16 || state.len != 1 || *(uint8_t *)state.buf > 15) {
+        PyErr_SetString(PyExc_ValueError, "Invalid data, key, IV, or state");
         return NULL;
     }
 
@@ -136,23 +77,8 @@ static PyObject *cbc(PyObject *args, uint8_t encrypt) {
     if (!PyArg_ParseTuple(args, "y*y*y*", &data, &key, &iv))
         return NULL;
 
-    if (data.len == 0) {
-        PyErr_SetString(PyExc_ValueError, "Data must not be empty");
-        return NULL;
-    }
-
-    if (data.len % 16 != 0) {
-        PyErr_SetString(PyExc_ValueError, "Data size must match a multiple of 16 bytes");
-        return NULL;
-    }
-
-    if (key.len != 32) {
-        PyErr_SetString(PyExc_ValueError, "Key size must be exactly 32 bytes");
-        return NULL;
-    }
-
-    if (iv.len != 16) {
-        PyErr_SetString(PyExc_ValueError, "IV size must be exactly 16 bytes");
+    if (data.len == 0 || data.len % 16 != 0 || key.len != 32 || iv.len != 16) {
+        PyErr_SetString(PyExc_ValueError, "Invalid data, key, or IV size");
         return NULL;
     }
 
@@ -178,55 +104,20 @@ static PyObject *cbc256_decrypt(PyObject *self, PyObject *args) {
     return cbc(args, 0);
 }
 
-PyDoc_STRVAR(
-    ige256_encrypt_docs,
-    "ige256_encrypt(data, key, iv)\n"
-    "--\n\n"
-    "AES-256-IGE Encryption"
-);
-
-PyDoc_STRVAR(
-    ige256_decrypt_docs,
-    "ige256_decrypt(data, key, iv)\n"
-    "--\n\n"
-    "AES-256-IGE Decryption"
-);
-
-PyDoc_STRVAR(
-    ctr256_encrypt_docs,
-    "ctr256_encrypt(data, key, iv, state)\n"
-    "--\n\n"
-    "AES-256-CTR Encryption"
-);
-
-PyDoc_STRVAR(
-    ctr256_decrypt_docs,
-    "ctr256_decrypt(data, key, iv, state)\n"
-    "--\n\n"
-    "AES-256-CTR Decryption"
-);
-
-PyDoc_STRVAR(
-    cbc256_encrypt_docs,
-    "cbc256_encrypt(data, key, iv)\n"
-    "--\n\n"
-    "AES-256-CBC Encryption"
-);
-
-PyDoc_STRVAR(
-    cbc256_decrypt_docs,
-    "cbc256_decrypt(data, key, iv)\n"
-    "--\n\n"
-    "AES-256-CBC Encryption"
-);
+PyDoc_STRVAR(ige256_encrypt_docs, "ige256_encrypt(data, key, iv)\nAES-256-IGE Encryption");
+PyDoc_STRVAR(ige256_decrypt_docs, "ige256_decrypt(data, key, iv)\nAES-256-IGE Decryption");
+PyDoc_STRVAR(ctr256_encrypt_docs, "ctr256_encrypt(data, key, iv, state)\nAES-256-CTR Encryption");
+PyDoc_STRVAR(ctr256_decrypt_docs, "ctr256_decrypt(data, key, iv, state)\nAES-256-CTR Decryption");
+PyDoc_STRVAR(cbc256_encrypt_docs, "cbc256_encrypt(data, key, iv)\nAES-256-CBC Encryption");
+PyDoc_STRVAR(cbc256_decrypt_docs, "cbc256_decrypt(data, key, iv)\nAES-256-CBC Encryption");
 
 static PyMethodDef methods[] = {
-    {"ige256_encrypt", (PyCFunction) ige256_encrypt, METH_VARARGS, ige256_encrypt_docs},
-    {"ige256_decrypt", (PyCFunction) ige256_decrypt, METH_VARARGS, ige256_decrypt_docs},
-    {"ctr256_encrypt", (PyCFunction) ctr256_encrypt, METH_VARARGS, ctr256_encrypt_docs},
-    {"ctr256_decrypt", (PyCFunction) ctr256_encrypt, METH_VARARGS, ctr256_decrypt_docs},
-    {"cbc256_encrypt", (PyCFunction) cbc256_encrypt, METH_VARARGS, cbc256_encrypt_docs},
-    {"cbc256_decrypt", (PyCFunction) cbc256_decrypt, METH_VARARGS, cbc256_decrypt_docs},
+    {"ige256_encrypt", (PyCFunction)ige256_encrypt, METH_VARARGS, ige256_encrypt_docs},
+    {"ige256_decrypt", (PyCFunction)ige256_decrypt, METH_VARARGS, ige256_decrypt_docs},
+    {"ctr256_encrypt", (PyCFunction)ctr256_encrypt, METH_VARARGS, ctr256_encrypt_docs},
+    {"ctr256_decrypt", (PyCFunction)ctr256_encrypt, METH_VARARGS, ctr256_decrypt_docs},
+    {"cbc256_encrypt", (PyCFunction)cbc256_encrypt, METH_VARARGS, cbc256_encrypt_docs},
+    {"cbc256_decrypt", (PyCFunction)cbc256_decrypt, METH_VARARGS, cbc256_decrypt_docs},
     {NULL}
 };
 
@@ -240,4 +131,4 @@ static struct PyModuleDef module = {
 
 PyMODINIT_FUNC PyInit_tgcrypto(void) {
     return PyModule_Create(&module);
-}
+    }
