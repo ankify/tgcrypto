fc56cd9e07b8cbfd29acdd7054a2b057dc1656f1
AnKiT <79446681+ankify@users.noreply.github.com>
2025-03-25 15:41:03 +0530

    Update tgcrypto.c

--- tgcrypto/tgcrypto.c
+++ tgcrypto/tgcrypto.c
@@ -7,7 +7,8 @@
 
 #define DESCRIPTION "Fast and Portable Cryptography Extension Library for Pyrogram"
 
-static PyObject *ige(PyObject *args, uint8_t encrypt) {
+// AES-256-IGE mode
+static PyObject *ige(PyObject *self, PyObject *args, uint8_t encrypt) {
     Py_buffer data, key, iv;
     uint8_t *buf;
     PyObject *out;
@@ -20,8 +21,15 @@ static PyObject *ige(PyObject *args, uint8_t encrypt) {
         return NULL;
     }
 
+    // Allocate memory for output buffer
+    buf = (uint8_t *)malloc(data.len);
+    if (!buf) {
+        PyErr_SetString(PyExc_MemoryError, "Memory allocation failed");
+        return NULL;
+    }
+
     Py_BEGIN_ALLOW_THREADS
-        buf = ige256(data.buf, data.len, key.buf, iv.buf, encrypt);
+        ige256(data.buf, buf, data.len, key.buf, iv.buf, encrypt);
     Py_END_ALLOW_THREADS
 
     PyBuffer_Release(&data);
@@ -35,13 +43,14 @@ static PyObject *ige(PyObject *args, uint8_t encrypt) {
 }
 
 static PyObject *ige256_encrypt(PyObject *self, PyObject *args) {
-    return ige(args, 1);
+    return ige(self, args, 1);
 }
 
 static PyObject *ige256_decrypt(PyObject *self, PyObject *args) {
-    return ige(args, 0);
+    return ige(self, args, 0);
 }
 
+// AES-256-CTR mode
 static PyObject *ctr256_encrypt(PyObject *self, PyObject *args) {
     Py_buffer data, key, iv, state;
     uint8_t *buf;
@@ -55,13 +64,21 @@ static PyObject *ctr256_encrypt(PyObject *self, PyObject *args) {
         return NULL;
     }
 
+    // Allocate memory for output buffer
+    buf = (uint8_t *)malloc(data.len);
+    if (!buf) {
+        PyErr_SetString(PyExc_MemoryError, "Memory allocation failed");
+        return NULL;
+    }
+
     Py_BEGIN_ALLOW_THREADS
-        buf = ctr256(data.buf, data.len, key.buf, iv.buf, state.buf);
+        ctr256(data.buf, buf, data.len, key.buf, iv.buf, state.buf);
     Py_END_ALLOW_THREADS
 
     PyBuffer_Release(&data);
     PyBuffer_Release(&key);
     PyBuffer_Release(&iv);
+    PyBuffer_Release(&state);
 
     out = Py_BuildValue("y#", buf, data.len);
     free(buf);
@@ -69,7 +86,8 @@ static PyObject *ctr256_encrypt(PyObject *self, PyObject *args) {
     return out;
 }
 
-static PyObject *cbc(PyObject *args, uint8_t encrypt) {
+// AES-256-CBC mode
+static PyObject *cbc(PyObject *self, PyObject *args, uint8_t encrypt) {
     Py_buffer data, key, iv;
     uint8_t *buf;
     PyObject *out;
@@ -82,8 +100,15 @@ static PyObject *cbc(PyObject *args, uint8_t encrypt) {
         return NULL;
     }
 
+    // Allocate memory for output buffer
+    buf = (uint8_t *)malloc(data.len);
+    if (!buf) {
+        PyErr_SetString(PyExc_MemoryError, "Memory allocation failed");
+        return NULL;
+    }
+
     Py_BEGIN_ALLOW_THREADS
-        buf = cbc256(data.buf, data.len, key.buf, iv.buf, encrypt);
+        cbc256(data.buf, buf, data.len, key.buf, iv.buf, encrypt);
     Py_END_ALLOW_THREADS
 
     PyBuffer_Release(&data);
@@ -97,30 +122,32 @@ static PyObject *cbc(PyObject *args, uint8_t encrypt) {
 }
 
 static PyObject *cbc256_encrypt(PyObject *self, PyObject *args) {
-    return cbc(args, 1);
+    return cbc(self, args, 1);
 }
 
 static PyObject *cbc256_decrypt(PyObject *self, PyObject *args) {
-    return cbc(args, 0);
+    return cbc(self, args, 0);
 }
 
+// Documentation strings
 PyDoc_STRVAR(ige256_encrypt_docs, "ige256_encrypt(data, key, iv)\nAES-256-IGE Encryption");
 PyDoc_STRVAR(ige256_decrypt_docs, "ige256_decrypt(data, key, iv)\nAES-256-IGE Decryption");
 PyDoc_STRVAR(ctr256_encrypt_docs, "ctr256_encrypt(data, key, iv, state)\nAES-256-CTR Encryption");
 PyDoc_STRVAR(ctr256_decrypt_docs, "ctr256_decrypt(data, key, iv, state)\nAES-256-CTR Decryption");
 PyDoc_STRVAR(cbc256_encrypt_docs, "cbc256_encrypt(data, key, iv)\nAES-256-CBC Encryption");
-PyDoc_STRVAR(cbc256_decrypt_docs, "cbc256_decrypt(data, key, iv)\nAES-256-CBC Encryption");
+PyDoc_STRVAR(cbc256_decrypt_docs, "cbc256_decrypt(data, key, iv)\nAES-256-CBC Decryption");
 
+// Method definitions
 static PyMethodDef methods[] = {
     {"ige256_encrypt", (PyCFunction)ige256_encrypt, METH_VARARGS, ige256_encrypt_docs},
     {"ige256_decrypt", (PyCFunction)ige256_decrypt, METH_VARARGS, ige256_decrypt_docs},
     {"ctr256_encrypt", (PyCFunction)ctr256_encrypt, METH_VARARGS, ctr256_encrypt_docs},
-    {"ctr256_decrypt", (PyCFunction)ctr256_encrypt, METH_VARARGS, ctr256_decrypt_docs},
     {"cbc256_encrypt", (PyCFunction)cbc256_encrypt, METH_VARARGS, cbc256_encrypt_docs},
     {"cbc256_decrypt", (PyCFunction)cbc256_decrypt, METH_VARARGS, cbc256_decrypt_docs},
-    {NULL}
+    {NULL, NULL, 0, NULL}
 };
 
+// Module definition
 static struct PyModuleDef module = {
     PyModuleDef_HEAD_INIT,
     "TgCrypto",
@@ -129,6 +156,7 @@ static struct PyModuleDef module = {
     methods
 };
 
+// Module initialization function
 PyMODINIT_FUNC PyInit_tgcrypto(void) {
     return PyModule_Create(&module);
-    }
+}
